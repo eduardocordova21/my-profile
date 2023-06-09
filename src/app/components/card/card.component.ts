f6069746a3b5825544799221bf20acb16fda4250
Eduardo Matheus Oliveira de Córdova <eduardocordova21@outlook.com.br>
2023-06-09 15:17:15 -0300

    Animando os cards das redes sociais

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { AfterContentChecked, Component, Input } from '@angular/core';
 import { ISocialNetwork } from 'src/app/models/models';
+import VanillaTilt from 'vanilla-tilt';
 
 @Component({
   selector: 'card',
   templateUrl: './card.component.html',
+  styleUrls: ['card.component.scss'],
 })
-export class CardComponent {
+export class CardComponent implements AfterContentChecked {
   @Input() public socialNetworks: ISocialNetwork[] = [];
+
+  public ngAfterContentChecked(): void {
+    let cards: any = document.querySelectorAll('#my-card');
+
+    VanillaTilt.init(cards, {
+      max: 25,
+      speed: 40,
+      glare: true,
+      'max-glare': 1,
+    });
+  }
 }
